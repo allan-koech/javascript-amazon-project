2ffb18d5adb91d4b05db534bb5c741f7d2758e36
allan-koech <allan.koech1@outlook.com>
2024-12-05 13:40:43 +0300

    make localStorage Key and load from storageKey private

--- data/cart-class.js
+++ data/cart-class.js
@@ -2,12 +2,12 @@ import { renderPaymentSummary } from "../scripts/checkout/paymentSummary.js";
 
 class Cart{
   cartItems;
-  localStorageKey;
+#localStorageKey;
   constructor(localStorageKey){
-this.loadFromStorageKey = localStorageKey;
-this.loadFromStorage();
+this.#localStorageKey= localStorageKey;
+this.#loadFromStorage();
   };
-  loadFromStorage(){
+#loadFromStorage(){
     this.cartItems=JSON.parse(localStorage.getItem(`${this.localStorageKey}`))||[{
       productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity:2,
